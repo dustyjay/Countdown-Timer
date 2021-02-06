6e2988716ba02c8bd3b2f100877a718f3b3de935
janearisah <janet@chaka.ng>
2021-02-06 17:19:36 +0100

    complete countdown mvp

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Countdown-Timer
 
-A simple NPM package for counting down time
+```
+NPM
+$ npm install @dustyjay/countdown-timer
+```
+
+```
+Using Yarn
+$ yarn add @dustyjay/countdown-timer
+```
+
+## [NPM Package](https://www.npmjs.com/package/@dustyjay/countdown-timer)
+
+Package Repo: [https://github.com/dustyjay/Countdown-Timer](https://github.com/dustyjay/Countdown-Timer)
+
+A light-weight package for a simple countdown timer
---
+++ index.js
@@ -0,0 +1,36 @@
+function Countdown(startDate) {
+  let returnObj = { cdDate: null };
+  const date = new Date(startDate);
+  if (isNaN(date.getTime())) throw new TypeError('Invalid Date!');
+  returnObj.cdDate = date;
+
+  function startCountdown() {
+    const { cdDate } = returnObj;
+    if (cdDate === null)
+      throw new RangeError(
+        'Use new Countdown("date-format") to set date before starting countdown'
+      );
+    var now = new Date();
+
+    var distance = new Date(cdDate).getTime() - now.getTime();
+
+    // If the count down is finished, return flag for completion
+    if (distance < 0) {
+      returnObj.cdDate = 0;
+      return { days: 0, hours: 0, minutes: 0, seconds: 0, isComplete: true };
+    }
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds, isComplete: false };
+  }
+
+  returnObj.startCountdown = startCountdown;
+  return returnObj;
+}
+
+exports.Countdown = Countdown;
